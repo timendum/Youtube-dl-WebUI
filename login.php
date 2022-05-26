8dc00c14d68bd7bc27f0d8c794524ab11b27be2b
Timendum <timedum@gmail.com>
2022-05-26 10:11:14 +0200

    Center login page

--- login.php
+++ login.php
@@ -29,27 +29,25 @@
 	<?php
 		}
 	?>
-	<div class="row my-3">
-		<div class="col-md-4"></div>
-		<div class="col-md-4">
-			<h2>Login:</h2>
-		</div>
-		<div class="col-md-4"></div>
-	</div>
-	<form class="form-horizontal" action="login.php" method="POST">
-		<div class="input-group my-3">
-			<div class="col-lg-4"></div>
-				<div class="col-lg-4">
-					<input class="form-control" id="password" name="password" placeholder="Password" type="password" />
-				</div>
-			<div class="col-lg-4"></div>
-		</div>
-		<div class="input-group my-3">
-			<div class="col-lg-4"></div>
-			<div class="col-lg-4">
-				<button type="submit" class="btn btn-primary">Sign in</button>
+	<form class="form-horizontal" action="login.php" method="POST" data-bitwarden-watching="1">
+		<div class="row my-3 justify-content-md-center">
+			<div class="col col-md-4">
+				<h2>Login</h2>
 			</div>
-			<div class="col-lg-4"></div>
+		</div>
+		<div class="row my-3 justify-content-md-center">
+			<div class="col col-lg-4 ">
+				<div class="input-group">
+					<input class="form-control" id="password" name="password" placeholder="Password" type="password">
+				</div>
+			</div>
+		</div>
+		<div class="row my-3 justify-content-md-center">
+			<div class="col col-lg-4">
+				<div class="input-group">
+					<button type="submit" class="btn btn-primary">Sign in</button>
+				</div>
+			</div>
 		</div>
 	</form>
 </div>
